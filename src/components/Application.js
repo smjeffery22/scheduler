d293dcbcb4356deae0d7d4c174401158f8f3d260
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-01-18 16:16:57 -0500

    Forgot to commit previous work. Enable storybook testing for Button and DayListItem

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 import "components/Application.scss";
-
+import Button from "./Button";Z
 export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu"></nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+        <Button />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames"
 
 export default function Button(props) {
-   return <></>;
+   // props change for different buttons
+   // props.children - anything between Button opening/closing tags in Storybook file for each button
+   let btnClass = classNames(
+      "button",
+      {
+         "button--confirm": props.confirm,
+         "button--danger": props.danger
+      }
+   )
+
+   return (
+
+      <button
+         className={btnClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
 }
