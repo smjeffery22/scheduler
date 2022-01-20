85f35bb5705806fc5b3c8c57d93a87fb2fa4c078
Jeffery Park <jp@Jefferys-MacBook-Pro.local>
2022-01-20 00:23:43 -0500

    Add Show and Empty components in Appointment component to display appointments

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Show(props) {
-  const { student, interviewers, onEdit, onDelete } = props;
+  const { student, interviewer, onEdit, onDelete } = props;
 
   return (
     <main className="appointment__card appointment__card--show">
@@ -9,7 +9,7 @@ function Show(props) {
         <h2 className="text--regular">{student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">{interviewers[0].name}</h3>
+          <h3 className="text--regular">{interviewer.name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 import "./styles.scss";
 
 function Appointment(props) {
-  const { time } = props;
+  const { time, interview } = props;
 
   return (
     <article className="appointment">
-      {time ? `Appointment at ${time}` : "No Appointments"}
+      <Header time={time} />
+      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
     </article>
   );
 }
