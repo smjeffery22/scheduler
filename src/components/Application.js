42496dc8c429c2a0334b4ef86ce97e00bd0af7fd
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-01-21 14:49:33 -0500

    Add selector function and its jest test, and use the function to render interviews

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,33 +2,40 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 import "components/Application.scss";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  
+
   const parsedAppointments = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return <Appointment
       key={appointment.id}
-      {...appointment}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+      // {...appointment}
     />
   })
 
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ])
       .then((all) => {
-        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
       })
       .catch((err) => console.log(err.message));
   }, []);
