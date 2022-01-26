95658b641dda943fe111438ad8128990e7d2d02a
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-01-25 22:50:57 -0500

    Implement error handling for empty form submission and its tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ function Form(props) {
   const { studentName, interviewerId, interviewers, onSave, onCancel } = props;
   const [student, setStudent] = useState(studentName || "");
   const [interviewer, setInterviewer] = useState(interviewerId || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("");
@@ -21,6 +22,15 @@ function Form(props) {
   // to prevent the form from submitting
   const handleSubmit = (e) => e.preventDefault();
 
+  const validate = () => {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    onSave(student, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -32,7 +42,9 @@ function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(e) => setStudent(e.target.value)}
+            data-testid="student-name-input"
           />
+        <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={interviewers}
@@ -43,7 +55,7 @@ function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
---
+++ src/components/__tests__/Form.test.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+import { render, cleanup, fireEvent, prettyDOM } from "@testing-library/react";
+
+import Form from "components/Appointment/Form";
+
+afterEach(cleanup);
+
+describe("Form", () => {
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    }
+  ];
+
+  // it("renders without student name if not provided", () => {
+  //   const { getByPlaceholderText } = render(<Form interviewers={interviewers} />);
+  //   expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  // });
+
+  // it("renders with initial student name", () => {
+  //   const { getByTestId } = render(<Form interviewers={interviewers} studentName="Jeffery Park" />);
+  //   expect(getByTestId("student-name-input")).toHaveValue("Jeffery Park");
+  // });
+
+  it("validates that the student name is not blank", () => {
+    /* 1. Create the mock onSave function */
+    const onSave = jest.fn();
+
+    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
+    const { getByText } = render(<Form interviewers={interviewers} onSave={onSave} />)
+
+    /* 3. Click the save button */
+    const saveButton = getByText("Save");
+    // console.log(prettyDOM(saveButton));
+    fireEvent.click(saveButton)
+
+    /* validation is shown */
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+
+    /* onSave is not called */
+    expect(onSave).not.toHaveBeenCalled();
+  });
+
+  it("calls onSave function when the name is defined", () => {
+    /* 1. Create the mock onSave function */
+    const onSave = jest.fn();
+
+    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
+    const { getByText, queryByText } = render(
+      <Form
+        interviewers={interviewers}
+        studentName="Jeffery Park"
+        onSave={onSave}
+      />)
+
+    /* 3. Click the save button */
+    const saveButton = getByText("Save");
+    fireEvent.click(saveButton)
+
+    /* validation is not shown */
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+    /* onSave is called once*/
+    expect(onSave).toHaveBeenCalledTimes(1);
+
+    /* onSave is called with the correct arguments */
+    expect(onSave).toHaveBeenCalledWith("Jeffery Park", null);
+  });
+});
